64b157b9caa0a3bf488bb5bf694e87d0dfb5af60
shafi <116013717+myselfShafi@users.noreply.github.com>
2025-11-25 22:55:09 +0530

    moved controller and routes file

--- src/ApiController.ts
+++ src/ApiController.ts
@@ -1,24 +1,24 @@
-import type { Request, Response } from 'express';
-import { asyncHandler } from './utils/index.js';
+import type { Request, Response } from "express";
 
-const handleGet = async (req: Request, res: Response) => {
+const handleGet = async (req:Request, res: Response) => {
     
 };
 
-const handlePost = asyncHandler(async (req: Request, res: Response) => {
+const handlePost = async (req:Request, res: Response) => {
     const { params : { collection }, path } = req;
     const basePath = path.split('/')[1];
 
     switch (basePath) {
         case 'create': {
-            
+            // const doc = await prisma.user.create({});
             return res.json({ message: `${collection} created!!`});
         }
     
         default:
             return res.json('switch case not found!')
     }
-});
+};
 
 
 export { handleGet, handlePost };
+
