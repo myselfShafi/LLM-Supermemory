e7e1b5a296c39e9496deb71e0fc265248a116fe7
shafi <116013717+myselfShafi@users.noreply.github.com>
2025-11-30 21:59:37 +0530

    controller setup for collection/create

--- src/ApiController.ts
+++ src/ApiController.ts
@@ -1,5 +1,8 @@
 import type { Request, Response } from "express";
+import { HTTP_STATUS } from "./constants/httpCodes.constant.js";
 import prisma from "./database/prisma.js";
+import ApiError from "./utils/ApiError.util.js";
+import ApiResponse from "./utils/ApiResponse.util.js";
 import asyncHandler from "./utils/asyncHandler.util.js";
 
 const handleGet = asyncHandler(async (req:Request, res: Response) => {
@@ -13,11 +16,14 @@ const DB_REGISTRY = {
 const handlePost = asyncHandler(async (req:Request, res: Response) => {
     const { params : { collection }, path, body } = req;
     const basePath = path.split('/')[2];
-
+    if (!collection) throw new ApiError(HTTP_STATUS.BAD_REQUEST, 'Invalid or missing collection name.');
+    console.log({collection, basePath, body});
+    
     switch (basePath) {
         case 'create': {
-            const doc = await prisma[collection].create({data: body.payload});
-            return res.json({ message: `${collection} created!!`});
+            const doc = await (prisma as any)[collection].create({ data: body.payload });
+            console.log({doc});
+            return new ApiResponse(HTTP_STATUS.OK, doc, `${collection} created!!`);
         }
     
         default:
